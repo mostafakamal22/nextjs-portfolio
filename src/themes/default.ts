69e9d2565af1383d0ab976e97e8b4265a31a257a
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-08-12 20:42:44 +0200

    fix global styles ts issue

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,24 +1,6 @@
-type ThemeTypes = {
-  fonts: {
-    title: string;
-    main: string;
-  };
-  colors: {
-    primary1: string;
-    background1: string;
-    accent1: string;
-    button: string;
-    background2: string;
-  };
-  breakpoints: {
-    sm: string;
-    md: string;
-    lg: string;
-    xl: string;
-  };
-};
+import { DefaultTheme } from "styled-components";
 
-const theme: ThemeTypes = {
+const theme: DefaultTheme = {
   // Temp fonts
   fonts: {
     title: "Space Grotesk, sans-serif",
@@ -41,4 +23,4 @@ const theme: ThemeTypes = {
   },
 };
 
-export default theme;
+export { theme };
