2dbf6ca7e4b8c9a786575ceaac9a4c87cc858b45
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-15 00:06:08 +0200

    improve about me section phrases

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 import { config, useTransition } from "react-spring";
 import {
   Section,
@@ -9,7 +9,7 @@ import {
 
 interface AboutMeProps {
   animateAbout: boolean;
-  setAnimateAbout(bol: boolean): void;
+  setAnimateAbout: Dispatch<SetStateAction<boolean>>;
 }
 
 const AboutMe = ({ animateAbout, setAnimateAbout }: AboutMeProps) => {
@@ -44,10 +44,13 @@ const AboutMe = ({ animateAbout, setAnimateAbout }: AboutMeProps) => {
         (styles, item) =>
           item && (
             <SectionText style={styles}>
-              I am Mostafa Kamal. <br />
-              I've a passion for web development, especially front-end web
-              development, modern technologies concerning interactive web page
-              experience, and a love of learning.{" "}
+              I have a passion for web development, including both front-end and
+              back-end technologies, with a particular focus on the MERN stack.
+              I love creating modern, interactive web experiences and stay
+              up-to-date on the latest tools and techniques to ensure the best
+              possible user experience. Learning is a constant priority for me,
+              and I am always looking for opportunities to enhance my skills and
+              knowledge.
             </SectionText>
           )
       )}
@@ -56,9 +59,12 @@ const AboutMe = ({ animateAbout, setAnimateAbout }: AboutMeProps) => {
         (styles, item) =>
           item && (
             <SectionText style={styles}>
-              I am a self-taught developer willing to learn and enhance my
-              experiences in the best way possible, always responsible, and
-              committed to my duties.{" "}
+              As a self-taught developer, I have a deep appreciation for the
+              value of continuous learning and professional growth. I am
+              committed to taking on new challenges and expanding my skillset in
+              order to deliver the best possible results for my clients. I
+              approach every project with a sense of responsibility and
+              dedication to excellence.
             </SectionText>
           )
       )}
